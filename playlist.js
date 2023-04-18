81db2366294714c0e9b83311a7b8593f5dd17e80
clara9999 <103687242+clara9999@users.noreply.github.com>
2023-04-18 16:12:53 +0800

    Create playlist.js

--- playlist.js
+++
@@ -1,4 +0,0 @@
-//YouTubeVideoID
-const playList=["SwZMS-ISbH4","SwZMS-ISbH4","SwZMS-ISbH4"];
-//播放起訖秒數
-const playTime=[[101,109],[140,148],[174,186]];
---
+++ 播放列表.js
@@ -0,0 +1,4 @@
+//YouTubeVideoID
+const playList=["zlTIextYnyQ","-agZb7JmvVM","YudHcBIxlYw"];
+//播放起訖秒數
+const playTime=[[42,63],[96,114],[65,75]];
